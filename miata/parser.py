b35b61024a22417795eabbf4577dad158acd842c
Alexander Lyashuk <lx.lyashuk@gmail.com>
2025-07-11 02:04:43 +0300

    Update parser.py

--- miata/parser.py
+++ miata/parser.py
@@ -1,17 +1,18 @@
-import requests
+import cloudscraper
 from bs4 import BeautifulSoup
 
 
 def get_from_bazaraki(
     url="https://www.bazaraki.com/car-motorbikes-boats-and-parts/cars-trucks-and-vans/mazda/mazda-mx5/",
 ):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    ads = soup.find_all("a", href=True, text=lambda t: t and "Mazda MX5" in t)
+    with cloudscraper.create_scraper() as scraper:
+        response = scraper.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        ads = soup.find_all("a", href=True, text=lambda t: t and "Mazda MX5" in t)
 
-    results = []
-    for tag in ads:
-        title = tag.text.strip()
-        link = "https://www.bazaraki.com" + tag["href"]
-        results.append((title, link))
-    return results
+        results = []
+        for tag in ads:
+            title = tag.text.strip()
+            link = "https://www.bazaraki.com" + tag["href"]
+            results.append((title, link))
+        return results
